460a2eae4d8709e71e90a0e959da47fe18059ee1
francisconroy <francis.conroy@uon.edu.au>
2016-07-18 12:38:48 +0200

    add new code for segment test
    Test code for segment powers each LED independently with a delay
    in-between. Code used to test update time and delay using high FPS go
    pro footage

---
+++ test_code/test_segment_4/test_segment_4.ino
@@ -0,0 +1,71 @@
+//something
+
+
+static int plexleda [] = {6,20,21,5,6,20,21,5,6,20,21,5,6,20,21,5};
+static int plexledc []= {10,10,10,10,13,13,13,13,11,11,11,11,12,12,12,12};
+
+static int del = 60;
+
+//void ledon(int number);
+
+void setup() 
+{
+  // put your setup code here, to run once:
+  for (int i=0;i<16;i++)
+  {
+    pinMode(plexleda[i], OUTPUT); //initialise the inputs
+    pinMode(plexledc[i], OUTPUT); //initialise the inputs
+  }
+  pinMode(0,OUTPUT);//status pin 
+  digitalWrite(0,HIGH);
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+  
+  ledon(0,plexleda,plexledc);
+  delay(del);
+  ledon(1,plexleda,plexledc);
+  delay(del);
+  ledon(2,plexleda,plexledc);
+  delay(del);
+  ledon(3,plexleda,plexledc);
+  delay(del);
+  ledon(4,plexleda,plexledc);
+  delay(del);
+  ledon(5,plexleda,plexledc);
+  delay(del);
+    ledon(6,plexleda,plexledc);
+  delay(del);
+    ledon(7,plexleda,plexledc);
+  delay(del);
+    ledon(8,plexleda,plexledc);
+  delay(del);
+    ledon(9,plexleda,plexledc);
+  delay(del);
+    ledon(10,plexleda,plexledc);
+  delay(del);
+    ledon(11,plexleda,plexledc);
+  delay(del);
+    ledon(12,plexleda,plexledc);
+  delay(del);
+    ledon(13,plexleda,plexledc);
+  delay(del);
+    ledon(14,plexleda,plexledc);
+  delay(del);
+    ledon(15,plexleda,plexledc);
+  delay(del);
+
+}
+
+void ledon(int number, int * plexleda, int * plexledc)
+{
+  for (int i=0;i<16;i++)
+  {
+   digitalWrite(plexleda[i],LOW);
+   digitalWrite(plexledc[i],LOW);
+  }
+  digitalWrite(plexledc[number],HIGH);
+   digitalWrite(plexleda[number],HIGH);
+}
